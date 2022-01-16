fdf2a07641df9b1826124f289fba128b888b503c
Gabriel Cavalheiro Barreto <gabrielcavalheiro@locus.software>
2022-01-16 16:46:57 -0300

    Feat: altera configurações de horario de idioma para portugues brasileiro e horario de brasilia. E adiciona configuração default de autenticação do rest framework

--- core/settings.py
+++ core/settings.py
@@ -106,9 +106,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "pt-br"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "America/Sao_Paulo"
 
 USE_I18N = True
 
@@ -126,3 +126,9 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "users.User"
+
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated", # make all endpoints private
+    ]
+}
