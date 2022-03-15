c007b367110b33f7c315d2953eb319204fdb7e56
Gabriel <ga3-cavalheiro@hotmail.com>
2022-03-14 21:49:23 -0300

    Refactor: refatora para não exibir a senha, para não salvar o id e exibi-lo e para atualizar o usuario por completo

--- users/serializers/user_serializer.py
+++ users/serializers/user_serializer.py
@@ -6,6 +6,7 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = [
+            "id",
             "email",
             "password",
             "username",
@@ -15,9 +16,22 @@ class UserSerializer(serializers.ModelSerializer):
             "is_staff",
             "is_active",
         ]
+        read_only = [
+            "id",
+        ]
+        extra_kwargs = {"password": {"write_only": True}}
 
     def create(self, validated_data):
         user = User.objects.create(**validated_data)
         user.set_password(validated_data["password"])
         user.save()
         return user
+
+    def update(self, instance, validated_data):
+        for attr, value in validated_data.items():
+            if attr == "password":
+                instance.set_password(value)
+            else:
+                setattr(instance, attr, value)
+        instance.save()
+        return instance
