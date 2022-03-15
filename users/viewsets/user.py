f46ec2608a2ba27b15e4fa47e3f8c4bcf27c893e
Gabriel <ga3-cavalheiro@hotmail.com>
2022-03-14 21:50:29 -0300

    Refactor: modifica para usar ModelViewSet, adiciona verificação de permissão para permissões diferentes para os metodos

--- users/viewsets/user.py
+++ users/viewsets/user.py
@@ -1,4 +1,4 @@
-from rest_framework.viewsets import GenericViewSet
+from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.decorators import action
 from rest_framework.response import Response
@@ -9,25 +9,22 @@ from users.serializers import UserSerializer
 from users.models import User
 
 
-class UserViewSet(GenericViewSet):
+class UserViewSet(ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    permission_classes_by_action = {"create": [AllowAny], "list": [AllowAny]}
+
+    def get_permissions(self):
+        try:
+            # return permission_classes depending on `action`
+            return [permission() for permission in self.permission_classes_by_action[self.action]]
+        except KeyError:
+            # action is not set return default permission_classes
+            return [permission() for permission in self.permission_classes]
 
     @action(methods=["GET"], detail=False, url_path="profile", permission_classes=[IsAuthenticated])
     def profile(self, request: HttpRequest):
         user = request.user
         serializer = self.get_serializer(user)
         return Response({"user": serializer.data}, status=status.HTTP_200_OK)
-
-    @action(methods=["POST"], detail=False, url_path="create", permission_classes=[AllowAny])
-    def create_user(self, request: HttpRequest):
-        new_user = request.data
-
-        serializer = self.get_serializer(data=new_user)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        print(type(serializer.data))
-        print(serializer.data["password"])
-        del serializer.data["password"]
-
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
